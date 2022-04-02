234641b1e8e08a4b87ef8174eafc1a78823f3142
x_x <s.xiuxing@foxmail.com>
2022-04-02 22:15:00 +0800

    WIP: jest

--- taro/pages/package/components/button/__tests__/button.test.tsx
+++ taro/pages/package/components/button/__tests__/button.test.tsx
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react';
 import { Button } from 'antd-taro';
-import * as React from 'react';
 
 const classPrefix = `adm-button`;
 
--- taro/pages/package/components/button/button.tsx
+++ taro/pages/package/components/button/button.tsx
@@ -1,6 +1,5 @@
 import * as Components from '@tarojs/components';
 import type { FC } from 'react';
-import * as React from 'react';
 
 export const Button: FC = ({ children }) => {
   return <Components.Button>{children}</Components.Button>;
