31ba2309233242625df94ad2bb7979fba80ffda5
x_x <s.xiuxing@foxmail.com>
2022-04-03 21:36:16 +0800

    chore: h5 构建产物输出路径

--- scripts/redirect2demo.js
+++ scripts/redirect2demo.js
@@ -1,6 +1,6 @@
 (function () {
   const DEMO_HOST_DEV = 'http://localhost:10086';
-  const DEMO_HOST_PROD = 'http://localhost:3000';
+  const DEMO_HOST_PROD = 'https://艺蔚.wang/h5';
 
   const { hostname, search } = window.location;
 
