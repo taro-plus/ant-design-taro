7ca20225426a5237eaa597906b67588fe377bd9e
Sushi Su <s.xiuxing@foxmail.com>
2022-04-07 08:48:50 +0800

    WIP: button

---
+++ taro/pages/package/components/button/demo/demo2.tsx
@@ -0,0 +1,57 @@
+/**
+ * demoUrl: ?redirect=/pages/package/components/button/demo/index
+ */
+
+import { Button, Space } from 'antd-taro';
+import { DemoBlock } from 'demo-components';
+import type { FC } from 'react';
+import React from 'react';
+
+const ButtonDemo2: FC = () => {
+  return (
+    <>
+      <DemoBlock title="带图标的按钮">
+        <Button>
+          <Space>
+            11
+            <span>搜索</span>
+          </Space>
+        </Button>
+      </DemoBlock>
+
+      <DemoBlock title="禁用状态">
+        <Space wrap>
+          <Button disabled>Disabled</Button>
+          <Button disabled color="primary">
+            Disabled
+          </Button>
+        </Space>
+      </DemoBlock>
+
+      <DemoBlock title="加载状态">
+        <Space wrap>
+          <Button loading color="primary" loadingText="正在加载">
+            Loading
+          </Button>
+          <Button loading>Loading</Button>
+        </Space>
+      </DemoBlock>
+
+      <DemoBlock title="不同类型圆角">
+        <Space wrap>
+          <Button shape="default" color="primary">
+            Default Button
+          </Button>
+          <Button block shape="rounded" color="primary">
+            Rounded Button
+          </Button>
+          <Button block shape="rectangular" color="primary">
+            Rectangular Button
+          </Button>
+        </Space>
+      </DemoBlock>
+    </>
+  );
+};
+
+export default ButtonDemo2;
--- taro/pages/package/components/button/demo/index.tsx
+++ taro/pages/package/components/button/demo/index.tsx
@@ -2,11 +2,14 @@ import { View } from '@tarojs/components';
 import type { FC } from 'react';
 import React from 'react';
 import ButtonDemo1 from './demo1';
+import ButtonDemo2 from './demo2';
 
 const ButtonPage: FC = () => {
   return (
     <View>
       <ButtonDemo1 />
+
+      <ButtonDemo2 />
     </View>
   );
 };
