8a4e3d4efb73c3eada132a3388dcd5b8b2753da0
Sushi Su <s.xiuxing@foxmail.com>
2022-05-19 15:29:23 +0800

    fix: 打包引用路径

--- scripts/babel-transform-imports.js
+++ scripts/babel-transform-imports.js
@@ -6,6 +6,10 @@ module.exports = function () {
           path.node.source.value = path.node.source.value.replace(/lib-utils/, '../../utils');
         }
 
+        if (path.node.source.value.startsWith('lib-utils/')) {
+          path.node.source.value = path.node.source.value.replace(/lib-components/, '../');
+        }
+
         if (path.node.source.value.endsWith('.less')) {
           path.node.source.value = path.node.source.value.replace(/\.less$/, '.css');
         }
