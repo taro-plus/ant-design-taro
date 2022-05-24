35d31b881af1a8e516c2948def9ae3e7f4a0acba
Sushi Su <s.xiuxing@foxmail.com>
2022-05-24 19:33:40 +0800

    chore: 新架构

--- site/main.tsx
+++ site/main.tsx
@@ -1,5 +1,4 @@
 import 'antd/dist/antd.variable.min.css';
-import 'github-markdown-css/github-markdown.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
