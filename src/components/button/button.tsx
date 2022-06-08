88fcf3e3f723e80454abaf48692d184b481881cd
Sushi Su <s.xiuxing@foxmail.com>
2022-06-08 22:18:48 +0800

    WIP: button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import { Button as TaroButton } from '@tarojs/components';
+import { Button as TaroButton, View } from '@tarojs/components';
 import classNames from 'classnames';
 import type { FC, ReactNode } from 'react';
 import { Loading } from '../../index';
@@ -9,16 +9,11 @@ import { mergeProps } from '../../utils/with-default-props';
 
 type OmitTaroButtonProps = 'size' | 'type' | 'plain';
 
-type Color = 'default' | 'primary' | 'success' | 'warning' | 'danger';
-type Fill = 'solid' | 'outline' | 'none';
-type Size = 'mini' | 'small' | 'middle' | 'large';
-type Shape = 'default' | 'rounded' | 'rectangular';
-
 export interface ButtonProps extends Omit<TaroButtonProps, OmitTaroButtonProps>, NativeProps {
-  color?: Color;
-  fill?: Fill;
-  size?: Size;
-  shape?: Shape;
+  color?: 'default' | 'primary' | 'success' | 'warning' | 'danger';
+  fill?: 'solid' | 'outline' | 'none';
+  size?: 'mini' | 'small' | 'middle' | 'large';
+  shape?: 'default' | 'rounded' | 'rectangular';
   block?: boolean;
   loadingText?: string;
   loadingIcon?: ReactNode;
@@ -36,7 +31,7 @@ const defaultProps: ButtonProps = {
 };
 
 export const Button: FC<ButtonProps> = (p) => {
-  const { size, ...props } = mergeProps(defaultProps, p);
+  const props = mergeProps(defaultProps, p);
 
   const loading = props.loading;
   const disabled = props.disabled || loading;
@@ -44,7 +39,6 @@ export const Button: FC<ButtonProps> = (p) => {
   return withNativeProps(
     props,
     <TaroButton
-      {...props}
       className={classNames(
         classPrefix,
         props.color ? `${classPrefix}-${props.color}` : null,
@@ -53,16 +47,22 @@ export const Button: FC<ButtonProps> = (p) => {
           [`${classPrefix}-disabled`]: disabled,
           [`${classPrefix}-fill-outline`]: props.fill === 'outline',
           [`${classPrefix}-fill-none`]: props.fill === 'none',
-          [`${classPrefix}-mini`]: size === 'mini',
-          [`${classPrefix}-small`]: size === 'small',
-          [`${classPrefix}-large`]: size === 'large',
+          [`${classPrefix}-mini`]: props.size === 'mini',
+          [`${classPrefix}-small`]: props.size === 'small',
+          [`${classPrefix}-large`]: props.size === 'large',
           [`${classPrefix}-loading`]: loading,
         },
         `${classPrefix}-shape-${props.shape}`,
       )}
     >
-      <Loading />
-      {props.children}
+      {loading ? (
+        <View className={`${classPrefix}-loading-wrapper`}>
+          {props?.loadingIcon ?? <Loading color="currentColor" size={props.size} />}
+          {props.loadingText}
+        </View>
+      ) : (
+        props.children
+      )}
     </TaroButton>,
   );
 };
--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,18 +1,42 @@
 import { Text, View } from '@tarojs/components';
+import classNames from 'classnames';
 import type { FC } from 'react';
 import type { NativeProps } from '../../utils/native-props';
 import { withNativeProps } from '../../utils/native-props';
+import { mergeProps } from '../../utils/with-default-props';
 
 type CSSProperties = '--color' | '--size';
 
-export type DotLoadingProps = NativeProps<CSSProperties>;
+export interface DotLoadingProps extends NativeProps<CSSProperties> {
+  color?: 'default' | 'primary' | 'white' | (string & {});
+  size?: 'mini' | 'small' | 'middle' | 'large';
+}
 
 const classPrefix = 'adt-loading';
 
-export const Loading: FC<DotLoadingProps> = (props) => {
+const defaultProps = {
+  color: 'default',
+};
+
+const colorRecord: Record<string, string> = {
+  default: 'var(--adm-color-weak)',
+  primary: 'var(--adm-color-primary)',
+  white: 'var(--adm-color-white)',
+};
+
+export const Loading: FC<DotLoadingProps> = (p) => {
+  const props = mergeProps(defaultProps, p);
+
   return withNativeProps(
     props,
-    <View className={classPrefix}>
+    <View
+      className={classNames(classPrefix, `${classPrefix}-${props.size}`)}
+      style={
+        {
+          '--color': colorRecord?.[props.color] ?? props.color,
+        } as unknown as CSSProperties
+      }
+    >
       <Text className={`${classPrefix}__dot`} />
       <Text className={`${classPrefix}__dot`} />
       <Text className={`${classPrefix}__dot`} />
